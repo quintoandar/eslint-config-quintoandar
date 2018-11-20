3d23ea8814743d3dde1d407bd9ae664ca86d0754
pamepeixinho <pameiupip@gmail.com>
2018-11-20 20:28:38 -0200

    adding base

--- eslint-config-quintoandar-base/index.js
+++ eslint-config-quintoandar-base/index.js
@@ -2,10 +2,18 @@ const rules = require('./rules/index');
 
 module.exports = {
   parser: 'babel-eslint',
+
   extends: [
     'eslint:recommended',
     'airbnb-base',
   ],
+
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true
+  },
 
   parserOptions: {
     ecmaVersion: 6,
