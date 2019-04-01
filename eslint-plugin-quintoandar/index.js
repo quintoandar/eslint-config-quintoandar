2467e3ad69e7b6e200a07580c593e4f4453e177b
Henrique Sosa <henriquesosa@gmail.com>
2019-04-01 18:09:54 -0300

    Create new style rule to block-party colors

--- eslint-plugin-quintoandar/index.js
+++ eslint-plugin-quintoandar/index.js
@@ -4,12 +4,14 @@ module.exports = {
     'no-dynamic-import-index': require('./rules/no-dynamic-import-index'),
     'no-target-blank': require('./rules/no-target-blank'),
     'no-typo-components': require('./rules/no-typo-components'),
+    'no-block-party-colors': require('./rules/no-block-party-colors'),
   },
   configs: {
     recommended: {
       rules: {
         'no-target-blank': 2,
         'no-typo-components': 2,
+        'no-block-party-colors': 2,
       },
     },
   },
--- eslint-plugin-quintoandar/rules/no-typo-components.js
+++ eslint-plugin-quintoandar/rules/no-typo-components.js
@@ -2,7 +2,7 @@ const notAllowedTypoPath = 'assets/typo';
 
 const reportText = `
   Do not use any of Block-party Typo components.
-  Use material-ui Typography (see: https://material-ui.com/api/typography/#typography) 
+  Use material-ui Typography (see: https://material-ui.com/api/typography/#typography)
   or Block-party Typography component (see: https://goo.gl/QP49NH) instead.
   Make sure to follow the guidelines to fix it (see: https://goo.gl/VwV911).
 `;
