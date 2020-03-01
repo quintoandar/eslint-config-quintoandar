d171a919838fc32d0c7327fe44d64640aadd8d62
zdafs <lucca.fonte@quintoandar.com.br>
2020-03-01 19:10:02 -0300

    Adding more rules

--- eslint-plugin-quintoandar/rules/custom-import-order.js
+++ eslint-plugin-quintoandar/rules/custom-import-order.js
@@ -1,28 +1,62 @@
-const cozyPath = /^@quintoandar\/cozy/;
-const biomaPath = /^@quintoandar\/bioma/;
+const reactPath = /^react$/;
+
+const externalLibsPaths = /^(?!(.\/|..\/|@quintoandar\/|components\/|containers\/|helpers\/|utils\/))/;
+
+const companyPaths = /^@quintoandar\//;
+
+const projectAbsolutePaths = /^(components\/|containers\/|helpers\/|utils\/)/;
+
+const relativePaths = /^(.\/|..\/)/;
 
 const importOrder = [
-  biomaPath,
-  cozyPath,
+  reactPath,
+  externalLibsPaths,
+  companyPaths,
+  projectAbsolutePaths,
+  relativePaths,
 ];
 
-const reportText = `
+const reportTextOutOfOrder = `
   The imports should follow the order:
-  ${biomaPath}
+
+  import ... from 'react';
+
+  import ... from 'external-lib';
+
+  import ... from '@quintoandar/';
+
+  import ... from 'components/';
+  import ... from 'containers/';
+  import ... from 'helpers/';
+  import ... from 'utils/';
+
+  import ... from '../';
+  import ... from './';
 `;
 
+const reportTextSortedAlphabetically = 'The imports should be sorted alphabetically';
+
 module.exports = function noHardcodedColors(context) {
-  let pastImportPosition = 0;
+  let pastImportPosition = -1;
+  let lastImportValue = '';
+
   return {
     ImportDeclaration(node) {
-      const newImportPosition = importOrder.findIndex((importPath) => importPath.test(node.source.value))
+      const currentImportValue = node.source.value;
+      const newImportPosition = importOrder.findIndex((importPath) => importPath.test(currentImportValue));
       if (newImportPosition >= 0 && pastImportPosition > newImportPosition) {
         context.report({
           node,
-          message: reportText,
+          message: reportTextOutOfOrder,
+        });
+      } else if (newImportPosition === pastImportPosition && lastImportValue > currentImportValue) {
+        context.report({
+          node,
+          message: reportTextSortedAlphabetically,
         });
       } else if (newImportPosition >= 0) {
         pastImportPosition = newImportPosition;
+        lastImportValue = currentImportValue;
       }
     }
   };
--- eslint-plugin-quintoandar/rules/tests/custom-import-order.test.js
+++ eslint-plugin-quintoandar/rules/tests/custom-import-order.test.js
@@ -20,37 +20,51 @@ const parserOptions = {
 // Tests
 // ------------------------------------------------------------------------------
 
-const cozyPath = /^@quintoandar\/cozy/;
-const biomaPath = /^@quintoandar\/bioma/;
-
-const importOrder = [
-  biomaPath,
-  cozyPath,
-];
-
-const reportText = `
+const reportTextOutOfOrder = `
   The imports should follow the order:
-  ${importOrder}
+
+  import ... from 'react';
+
+  import ... from 'external-lib';
+
+  import ... from '@quintoandar/';
+
+  import ... from 'components/';
+  import ... from 'containers/';
+  import ... from 'helpers/';
+  import ... from 'utils/';
+
+  import ... from '../';
+  import ... from './';
 `;
 
-console.log(reportText);
-
-const errors = [{ reportText }];
+const reportTextSortedAlphabetically = 'The imports should be sorted alphabetically';
 
 const validCode = `
-  import SomeUnrelatedComponent from '@quintoandar/bioma-unrelated/SomeUnrelatedComponent';
+  import React from 'react';
 
-  import SomeComponent from '@quintoandar/cozy-core/SomeComponent';
+  import something from 'external-lib/something-a';
+  import anotherThing from 'external-lib/something-b';
 
-  import Something from 'block-party/Something';
+  import SomeBiomaComponent from '@quintoandar/bioma-component-a/SomeComponent';
+  import AnotherBiomaComponent from '@quintoandar/bioma-component-b/AnotherComponent';
+  import SomeCozyComponent from '@quintoandar/cozy-component-a/SomeComponent';
+  import AnotherCozyComponent from '@quintoandar/cozy-component-b/AnotherComponent';
+
+  import SomeInternalComponent from 'components/SomeComponent';
+  import SomeInternalContainer from 'containers/SomeContainer';
+  import someInternalHelper from 'helpers/someHelper';
+  import someInternalUtil from 'utils/someUtil';
+
+  import SomethingFromRelativePath from '../../Something';
+  import AnotherThingFromRelativePath from '../AnotherThing';
+  import AndOneMoreThingFromRelativePath from './AndOneMoreThing';
 `;
 
 const invalidCode = `
-  import SomeComponent from '@quintoandar/cozy-core/SomeComponent';
+  import SomeComponent from 'some-component';
 
-  import Something from 'block-party/Something';
-
-  import SomeUnrelatedComponent from '@quintoandar/bioma-unrelated/SomeUnrelatedComponent';
+  import React from 'react';
 `;
 
 const ruleTester = new RuleTester({ parserOptions });
@@ -60,6 +74,6 @@ ruleTester.run('custom-import-order', rule, {
   ],
   invalid: [{
     code: invalidCode,
-    errors,
+    errors: [{ reportTextOutOfOrder }],
   }]
 });
